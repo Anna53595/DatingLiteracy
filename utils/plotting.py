be32acecdf5262b7926f0be5e37ad1fb726e5cc4
Anna Vollweiter <anna.vollweiter@cognostics.de>
2023-01-11 10:18:35 +0100

    modified plots and naive bayes (probably with bug)

--- utils/plotting.py
+++ utils/plotting.py
@@ -5,6 +5,6 @@ import pandas as pd
 def plot_question_distribution(question: str, data: pd.DataFrame, questions: pd.DataFrame):
     """plots histogram of polical orientation for a given question"""
     sns.catplot(
-        data=data, y=question, hue='q212813', kind="count",
+        data=data, y='q212813', hue=question, kind="count",
         palette="pastel", edgecolor=".6"
         ).set(title=questions.loc[question, 'text'])
